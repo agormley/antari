d19cae146534ae72b6abf50dc0194eb072cb1b1c
Andrew Gormley <andrewgormley@gmail.com>
2016-10-23 12:43:36 -0700

    add spaces

--- addressing.h
+++ addressing.h
@@ -1,17 +1,24 @@
 BYTE
 getImmediate(ushort addr);
+
 BYTE
 getZero(ushort addr, ushort* mid);
+
 BYTE
 getZeroX(ushort addr, ushort* mid);
+
 BYTE
 getAbsolute(ushort addr, ushort* mid);
+
 BYTE
 getAbsoluteX(ushort addr, ushort* mid);
+
 BYTE
 getAbsoluteY(ushort addr, ushort* mid);
+
 BYTE
 getIndirectX(ushort addr, ushort* mid);
+
 BYTE
 getIndirectY(ushort addr, ushort* mid);
 
